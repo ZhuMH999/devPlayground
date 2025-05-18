27fd3c00916d966a657ec85b2ea581f9b372169d
Ryan <zhumh.ryan@gmail.com>
2025-05-18 16:09:25 +0800

    new features: longDivision.py code now formats the strings and adds: 1. + and - symbols 2. superscript comments added in longDivision.py

--- longDivision.py
+++ longDivision.py
@@ -1,4 +1,17 @@
+"""
+Long division of polynomials made in Python by ZhuMH999
+
+To use:
+Modify the values in the SQUARE BRACKETS on line 50. DO NOT MODIFY ANYTHING ELSE.
+The format is as such: coeffient of highest power, coefficient of second highest power, ... constant
+If the power is non existant, put a 0. Include negative signs.
+
+The code can be run in online python IDEs.
+"""
+
 import copy
+
+SUP = str.maketrans("0123456789", "⁰¹²³⁴⁵⁶⁷⁸⁹")
 
 def polynomialDivision(divident, divisor):
     quotient = []
@@ -6,19 +19,34 @@ def polynomialDivision(divident, divisor):
     while len(remainder) >= len(divisor):
         temp = len(remainder) - len(divisor)
         minus_thing = [div * (remainder[0] / divisor[0]) for div in divisor] + temp * [0]
-        quotient += [remainder[0] / divisor[0]]
+        quotient += [str(remainder[0] / divisor[0])]
 
         for i in range(len(minus_thing)):
             remainder[i] -= minus_thing[i]
 
         remainder.pop(0)
 
-    formatted_quotient = [str(quotient[i]) + f'x^{len(quotient) - 1 - i}' for i in range(len(quotient))]
-    formatted_remainder = [str(remainder[i]) + f'x^{len(remainder) - 1 - i}' for i in range(len(remainder))]
+    remainder = [str(re) for re in remainder]
+
+    formatted_quotient = format_equation(quotient)
+    formatted_remainder = format_equation(remainder)
 
     return formatted_quotient, formatted_remainder
 
 
-q, r = polynomialDivision([1, -10, 27, -46, 28], [1, -7])
+def format_equation(equation):
+    formatted = [equation[i].replace('-', '- ') + f'x{str(len(equation) - 1 - i).translate(SUP)}' for i in range(len(equation))]
+    for f in range(len(formatted)):
+        if f == len(formatted) - 2:
+            formatted[f] = formatted[f][:-1]
+        elif f == len(formatted) - 1:
+            formatted[f] = formatted[f][:-2]
+
+        if '-' not in formatted[f] and f > 0:
+            formatted[f] = '+ ' + formatted[f]
+    return formatted
+
+
+q, r = polynomialDivision([2, 0, -1, 1, 0, -4], [1, -1, -2])
 print(f'Quotient = {" ".join(q)}')
 print(f'Remainder = {" ".join(r)}')
