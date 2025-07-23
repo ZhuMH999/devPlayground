cf18f6da3eab30aabd077ed0223aca14f992dabb
Ryan <zhumh.ryan@gmail.com>
2025-07-23 19:33:17 +0800

    minor changes: removed + improved bits of code in partialFractions

--- partialFractions.py
+++ partialFractions.py
@@ -1,6 +1,5 @@
 def partial_fractions(top, linear, repeated):
     sub_a = (-1 * linear[1]) / linear[0]
-    print(sub_a)
     left_a = sub_values(top, sub_a)
     a = left_a / ((repeated[0] * sub_a + repeated[1]) ** 2)
 
@@ -9,7 +8,7 @@ def partial_fractions(top, linear, repeated):
     c = left_c / (linear[0] * sub_c + linear[1])
 
     sub_b = 10
-    left_b = sub_values(top, sub_b) - ((sub_values(repeated, sub_b) ** 2) * a + c * sub_values(linear, sub_b))
+    left_b = round(sub_values(top, sub_b) - ((sub_values(repeated, sub_b) ** 2) * a + c * sub_values(linear, sub_b)), 2)
     b = left_b / (sub_values(linear, sub_b) * sub_values(repeated, sub_b))
     print(f'a = {a}, b = {b}, c = {c}')
 
@@ -23,4 +22,4 @@ def sub_values(polynomial, value):
 
     return total
 
-partial_fractions([3, -11, 5], [1, -2], [1, -1])
+partial_fractions([2, 7, -2], [1, -2], [1, 3])
