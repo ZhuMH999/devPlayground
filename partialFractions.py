acaa05de3998e14369cf2e45e2e716ec9e92ba5d
Ryan <zhumh.ryan@gmail.com>
2025-07-24 19:00:15 +0800

    new features: partialFractions accepts nonrepeated quadratic form

--- partialFractions.py
+++ partialFractions.py
@@ -3,30 +3,46 @@ def partial_fractions(top, factor1, factor2, case):
     left_a = sub_values(top, sub_a)
     if case == 'repeated':
         a = left_a / ((factor2[0] * sub_a + factor2[1]) ** 2)
-    elif case == 'linear':
+    elif case == 'nonrepeated':
+        a = left_a / (factor2[0] * (sub_a ** 2) + factor2[2])
+    else:
         a = left_a / (factor2[0] * sub_a + factor2[1])
-    else:  # case == 'nonrepeated'
-        print('unable to do this form (nonrepeated) yet.')
-        return
 
-    sub_c = (-1 * factor2[1]) / factor2[0]
-    left_c = sub_values(top, sub_c)
-    c = left_c / (factor1[0] * sub_c + factor1[1])
+    if case == 'nonrepeated':  # (x+2)(x^2+0x+1)
+        sub_c = 0
+        left_c = sub_values(top, sub_c)
+        c = (left_c - (a * sub_values(factor2, sub_c))) / (factor1[0] * sub_c + factor1[1])
+
+    else:
+        sub_c = (-1 * factor2[1]) / factor2[0]
+        left_c = sub_values(top, sub_c)
+        c = left_c / (factor1[0] * sub_c + factor1[1])
 
     if case != 'linear':
-        sub_b = check_for_divisable(factor1, factor2)
-        left_b = round(sub_values(top, sub_b) - ((sub_values(factor2, sub_b) ** 2) * a + c * sub_values(factor1, sub_b)), 2)
-        b = left_b / (sub_values(factor1, sub_b) * sub_values(factor2, sub_b))
+        sub_b = check_for_divisable(factor1, factor2, top, a, c, case)
+        print(sub_b)
+        if case == 'repeated':
+            left_b = round(sub_values(top, sub_b) - ((sub_values(factor2, sub_b) ** 2) * a + c * sub_values(factor1, sub_b)), 2)
+            b = round(left_b / (sub_values(factor1, sub_b) * sub_values(factor2, sub_b)), 2)
+
+        else:
+            left_b = round(sub_values(top, sub_b) - (sub_values(factor2, sub_b) * a + c * sub_values(factor1, sub_b)), 2)
+            b = round(left_b / (sub_b * sub_values(factor1, sub_b)), 2)
 
         print(f'a = {a}, b = {b}, c = {c}, case = {case}')
     else:
         print(f'a = {a}, b = {c}, case = {case}')
 
-def check_for_divisable(factor1, factor2):
+def check_for_divisable(factor1, factor2, top, a, c, case):
     stepper = 0
     while True:
         if sub_values(factor1, stepper) != 0 and sub_values(factor2, stepper) != 0:
-            return stepper
+            if case == 'repeated':
+                if round(sub_values(top, stepper) - ((sub_values(factor2, stepper) ** 2) * a + c * sub_values(factor1, stepper)), 2) != 0:
+                    return stepper
+            elif case == 'nonrepeated':
+                if round(sub_values(top, stepper) - (sub_values(factor2, stepper) * a + c * sub_values(factor1, stepper)), 2) != 0:
+                    return stepper
         stepper += 1
 
 
