e77e896a725e3f5c011a8f39d0b61327c3a4f8fc
Ryan <zhumh.ryan@gmail.com>
2025-07-24 17:42:03 +0800

    new features: partialFractions now checks for the type of case it is dealing with bugfix: fixed bug in partialFractions where certain inputs of polynomials can lead to wrong interpretations of the polynomial

--- partialFractions.py
+++ partialFractions.py
@@ -1,16 +1,34 @@
-def partial_fractions(top, factor1, factor2):
+def partial_fractions(top, factor1, factor2, case):
     sub_a = (-1 * factor1[1]) / factor1[0]
     left_a = sub_values(top, sub_a)
-    a = left_a / ((factor2[0] * sub_a + factor2[1]) ** 2)
+    if case == 'repeated':
+        a = left_a / ((factor2[0] * sub_a + factor2[1]) ** 2)
+    elif case == 'linear':
+        a = left_a / (factor2[0] * sub_a + factor2[1])
+    else:  # case == 'nonrepeated'
+        print('unable to do this form (nonrepeated) yet.')
+        return
 
     sub_c = (-1 * factor2[1]) / factor2[0]
     left_c = sub_values(top, sub_c)
     c = left_c / (factor1[0] * sub_c + factor1[1])
 
-    sub_b = 10
-    left_b = round(sub_values(top, sub_b) - ((sub_values(factor2, sub_b) ** 2) * a + c * sub_values(factor1, sub_b)), 2)
-    b = left_b / (sub_values(factor1, sub_b) * sub_values(factor2, sub_b))
-    print(f'a = {a}, b = {b}, c = {c}')
+    if case != 'linear':
+        sub_b = check_for_divisable(factor1, factor2)
+        left_b = round(sub_values(top, sub_b) - ((sub_values(factor2, sub_b) ** 2) * a + c * sub_values(factor1, sub_b)), 2)
+        b = left_b / (sub_values(factor1, sub_b) * sub_values(factor2, sub_b))
+
+        print(f'a = {a}, b = {b}, c = {c}, case = {case}')
+    else:
+        print(f'a = {a}, b = {c}, case = {case}')
+
+def check_for_divisable(factor1, factor2):
+    stepper = 0
+    while True:
+        if sub_values(factor1, stepper) != 0 and sub_values(factor2, stepper) != 0:
+            return stepper
+        stepper += 1
+
 
 def sub_values(polynomial, value):
     total = 0
@@ -23,44 +41,53 @@ def sub_values(polynomial, value):
     return total
 
 def get_coeff_of_polynomial(string, check_case=False):
+    case = None
+    if check_case:
+        case = 'linear'
+        if ')^' in string:
+            case = 'repeated'
+        elif '^' in string:
+            case = 'nonrepeated'
+
     splitted = string.split(')(')
     final = []
-    for i in range(len(splitted)):
-        term = splitted[i].split('+')
+
+    for part in splitted:
+        part = part.strip()
+
+        part = part.replace('-', '/+-')
+        terms = part.split('+')
+
         polynomial = []
-        for j in range(len(term)):
-            if len(term[j].split('-')) > 1:
-                polynomial.append(term[j].split('-')[0].split('x')[0])
-                for k in range(len(term[j].split('-')) - 1):
-                    polynomial.append('-' + term[j].split('-')[k+1].split('x')[0])
+        for term in terms:
+            term = term.strip()
+
+            if term == '' or term == '/':
+                continue
+            if '(' in term:
+                term = term.split('(')[-1]
+            if ')' in term:
+                term = term.split(')')[0]
+            if 'x' in term:
+                coeff = term.split('x')[0]
+                if coeff == '':
+                    coeff = '1'
+                elif coeff == '-':
+                    coeff = '-1'
             else:
-                polynomial.append(term[j].split('x')[0])
+                coeff = term
+            if coeff == '' or coeff == '-':
+                coeff += '1'
+
+            polynomial.append(int(coeff))
+
         final.append(polynomial)
-    return cleanup_polynomial(final, check_case)
+    return final, case
 
-def cleanup_polynomial(string, check_case=False):
-    case = 'linear'
-    for i in range(len(string)):
-        for j in range(len(string[i])):
-            if len(string[i][j].split('^')) > 1:
-                case = 'repeated'
-            if len(string[i][j].split('(')) > 1:
-                string[i][j] = string[i][j].split('(')[1].split(')')[0]
-            else:
-                string[i][j] = string[i][j].split('(')[0].split(')')[0]
-            if string[i][j] == '' or string[i][j] == '-':
-                string[i][j] += '1'
+print('Enter polynomials for the numerator in the form ax^2+bx+c.')
+numerator, _ = get_coeff_of_polynomial(input('Please enter the numerator with no spaces. >'))
 
-            string[i][j] = int(string[i][j])
-            print(type(string[i][j]))
+print('Enter polynomials for the denominator in the forms:\n\nNonrepeated Linear: (ax+b)(cx+d)\nRepeated Linear: (ax+b)(cx+d)^2\nNon-factorisable Quadratic: (ax+b)(cx^2+dx+e)')
+denominator, cas = get_coeff_of_polynomial(input('\nPlease enter denominator with no spaces >'), True)
 
-    if check_case:
-        print(string)
-        return string, case
-    print(string)
-    return string, None
-
-numerator, temp = get_coeff_of_polynomial(input('Please enter numerator in the form ax^2+bx+c, with no spaces. >'))
-denumerator, temp = get_coeff_of_polynomial(input('Please enter denominator in the form (ax+b)(cx+d)^2 with no spaces >'), True)
-
-print(partial_fractions(numerator[0], denumerator[0], denumerator[1]))
+partial_fractions(numerator[0], denominator[0], denominator[1], cas)
